0bb6d54469d6a4556e63548df44d0528c60abe28
Yuya Sato <yuyas203@gmail.com>
2018-10-29 21:41:09 +0900

    dropboxからファイルの内容を取得するスクリプト

---
+++ src/getfilecontents.js
@@ -0,0 +1,22 @@
+const Dropbox = require("dropbox").Dropbox;
+const { StringDecoder } = require("string_decoder");
+
+const dbx = new Dropbox({
+	accessToken: process.env.DBX_ACCESS_TOKEN,
+	fetch: require("isomorphic-fetch")
+});
+const FILE_PATH = "/Applications/Shortcut/medicine-check.txt";
+const decoder = new StringDecoder("utf8");
+
+const getFileContents = async () => {
+	try {
+		const meta = await dbx.filesDownload({ path: FILE_PATH });
+		return decoder.write(meta.fileBinary);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+module.exports = {
+	getFileContents
+};
