0c7ef301818143835525c96847f1df8d46a52b55
Yuya Sato <yuyas203@gmail.com>
2018-10-30 15:59:04 +0900

    asyncに変更し、デバッグ用のタスクも追加

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 const cron = require("node-cron");
+const moment = require("moment");
+moment().locale("ja");
 const { reply } = require("./src/reply");
 
 /** 起動確認 */
 console.log("Schedule execute...");
 
 /** 午前のタスク */
-cron.schedule("00 00 9 * * *", () => {
+cron.schedule("00 00 9 * * *", async () => {
 	console.log(moment().format("YYYY/MM/DD HH:mm:ss"));
-	isTakeMedicine("AM") ? console.log("ちゃんと飲んでる") : reply();
+	const result = await isTakeMedicine("AM");
+	result ? console.log("ちゃんと飲んでる") : reply();
 });
 
 /** 午後のタスク */
-cron.schedule("00 00 23 * * *", () => {
+cron.schedule("00 00 23 * * *", async () => {
 	console.log(moment().format("YYYY/MM/DD HH:mm:ss"));
-	isTakeMedicine("PM") ? console.log("ちゃんと飲んでる") : reply();
+	const result = await isTakeMedicine("PM");
+	resutl ? console.log("ちゃんと飲んでる") : reply();
+});
+
+/** デバッグ用のタスク */
+cron.schedule("00 00 * * * *", async () => {
+	console.log(moment().format("YYYY/MM/DD HH:mm:ss"));
+	console.log("稼働中");
 });
